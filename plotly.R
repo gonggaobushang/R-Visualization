c7237220abd68a35b5d17ab78ae935d72dd79e1d
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-07-15 15:03:37 +0800

    Update plotly.R

--- plotly.R
+++ plotly.R
@@ -164,3 +164,49 @@ df3 %>%
   animation_slider(
     hide = F
   )
+
+
+
+
+
+
+#能够截取部分长度
+library(plotly)
+library(quantmod)
+getSymbols(Symbols = c("AAPL", "MSFT"))
+
+ds <- data.frame(Date = index(AAPL), AAPL[,6], MSFT[,6])
+
+plot_ly(ds, x = ~Date) %>%
+  add_lines(y = ~AAPL.Adjusted, name = "Apple") %>%
+  add_lines(y = ~MSFT.Adjusted, name = "Microsoft") %>%
+  layout(
+    title = "Stock Prices",
+    xaxis = list(
+      rangeselector = list(
+        buttons = list(
+          list(
+            count = 3,
+            label = "3 mo",
+            step = "month",
+            stepmode = "backward"),
+          list(
+            count = 6,
+            label = "6 mo",
+            step = "month",
+            stepmode = "backward"),
+          list(
+            count = 1,
+            label = "1 yr",
+            step = "year",
+            stepmode = "backward"),
+          list(
+            count = 1,
+            label = "YTD",
+            step = "year",
+            stepmode = "todate"),
+          list(step = "all"))),
+      
+      rangeslider = list(type = "date")),
+    
+    yaxis = list(title = "Price"))
